9414063ade43fe8371d578de1120661ecc16b0e7
Ellery Macbook <eprisk.dev@gmail.com>
2014-07-12 07:26:27 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DartScore::Application.config.secret_key_base = '124170bcfb275930e2a1337c7c571ac8eb458ed06ee3a71ee435e720590a9bfc4ef73f9e9450c299e695aaf013f105b27cc483fd7f86d4322fd25958fd2ccec7'
