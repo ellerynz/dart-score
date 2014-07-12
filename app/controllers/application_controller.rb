ea8f64a39cba0cfa94c02292481f2e0c24eacb15
Ellery Macbook <eprisk.dev@gmail.com>
2014-07-12 16:48:36 +1200

    Generate random secret token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter { response.content_type = 'application/xhtml+xml' }  
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DartScore::Application.config.secret_key_base = '124170bcfb275930e2a1337c7c571ac8eb458ed06ee3a71ee435e720590a9bfc4ef73f9e9450c299e695aaf013f105b27cc483fd7f86d4322fd25958fd2ccec7'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+DartScore::Application.config.secret_key_base = secure_token
